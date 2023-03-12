5a438f1f4440074f1e0df2383d671f1b8d953a5a
Bryce <z1547440237@gmail.com>
2023-03-13 00:41:42 +0800

    导航栏初始

---
+++ MyFirstWebsite/vue-project/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Navbar from './components/Navbar.vue'
+const navContentobj = { 
+    first: "我",
+    second: "健身",
+    third: "手工",
+    forth: "翻译",
+    fifth: "猫"
+}
+console.log(navContentobj);
+</script>
+<template>
+    <div>
+    <Navbar :item="navContentobj"></Navbar>
+    </div>
+</template>
---
+++ MyFirstWebsite/vue-project/src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<script setup>
+const props = defineProps(["item"])
+</script>
+<template>
+    <div class="wrapper">
+        <div class="logo">
+            mylog
+        </div>
+        <div class="navContent" v-for="navitem in item">
+            {{ navitem }}
+        </div>
+    </div>
+</template>
+<style scoped>
+.wrapper {
+    background-color: #bfa;
+    display: flex;
+    width: 100%;
+    padding: px;
+}
+.logo {
+background-color: red;
+width: 15%;
+text-align: center;
+ color: white;
+  padding: 10px;
+  cursor:pointer;
+}
+.navContent {
+    box-sizing: content-box;
+    background-color: blue;
+    width: 10%;
+    flex: auto;
+    text-align: center;
+    color: white;
+    padding: 10px;
+    cursor:pointer;
+    margin-left: 1em ;
+}
+.navContent:hover {
+      box-shadow: 10px 10px 5px grey;
+      color:#bfa;
+      transform: scale(1.9);
+}
+</style>
