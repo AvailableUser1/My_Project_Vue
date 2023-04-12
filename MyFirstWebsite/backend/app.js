8efe03af950950b3f78ad68186cd68ab1ed01075
Bryce <z1547440237@gmail.com>
2023-04-12 15:15:43 +0800

    连接mysql

--- MyFirstWebsite/backend/app.js
+++ MyFirstWebsite/backend/app.js
@@ -1,9 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000; // 指定端口号
-app.listen(port, () => {
-    console.log(`Express app listening at http://localhost:${port}`);
+
+const mysql = require("mysql2");
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "test",
 });
-app.get('/', (req, res) => {
-    res.send('Hello Express!');
+
+connection.query("SELECT * FROM student_info", (err, results, fields) => {
+  if (err) {
+    console.error("查询失败: " + err.stack);
+    return;
+  }
+  console.log("查询结果：", results);
+  // 在这里可以处理查询结果
+});
+
+app.listen(port, () => {
+  console.log(`Express app listening at http://localhost:${port}`);
+});
+app.get("/", (req, res) => {
+  res.send("Hello Express!");
 });
